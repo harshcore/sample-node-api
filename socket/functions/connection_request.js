f78d6dd963fa588cbfb7feb36f1328fcff924989
Harsh Narwariya <h.narwariya@thesynapses.com>
2024-08-27 17:06:42 +0530

    socket updates

--- socket/functions/connection_request.js
+++ socket/functions/connection_request.js
@@ -1,4 +1,5 @@
 const { get_io } = require("..");
+const DirectMessage = require("../../models/DirectMessage");
 const User = require("../../models/User");
 const SOCKET_EVENT = require("../event.types");
 
@@ -6,12 +7,32 @@ const connection_request_functions = {
   new_connection_added: async (connection) => {
     io = get_io();
 
+    // io.to(connection.requester._id.toString())
+    //   .to(connection.recipient._id.toString())
+    //   .emit(SOCKET_EVENT.NEW_CONNECTION_ADDED);
+
     const user1 = await User.findById(connection.requester);
     const user2 = await User.findById(connection.recipient);
+
+    const last_message = await DirectMessage.findById(
+      connection.last_message._id
+    );
+
+    const last_message_data = {
+      _id: last_message._id,
+      content: last_message.content,
+      sender: last_message.sender,
+      receiver: last_message.receiver,
+      status: last_message.status,
+      createdAt: last_message.createdAt,
+      updatedAt: last_message.updatedAt,
+    };
 
     const connection_data1 = {
       _id: connection._id,
       status: connection.status,
+      updatedAt: connection.updatedAt,
+      last_message: last_message_data,
       user: {
         _id: user2._id,
         email: user2.email,
@@ -24,6 +45,8 @@ const connection_request_functions = {
     const connection_data2 = {
       _id: connection._id,
       status: connection.status,
+      updatedAt: connection.updatedAt,
+      last_message: last_message_data,
       user: {
         _id: user1._id,
         email: user1.email,
